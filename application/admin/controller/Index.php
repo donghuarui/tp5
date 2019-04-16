2e7167505200ee6ab4a95b9d62dfb0aa8a897ff2
董华锐 <ezreal.ma@foxmail.com>
2019-04-16 17:56:15 +0800

    1. 编写 crud

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -4,6 +4,7 @@ namespace app\admin\controller;
 
 use think\Controller;
 use think\Db;
+use app\admin\model\User;
 
 class Index extends Controller
 {
@@ -11,11 +12,74 @@ class Index extends Controller
     {
         return 'index/index';
     }
+
     //1、全局配置 config/database.php配置
-    public function dbTest()
+    public function dbFind1()
     {
-        $data = Db::table('user')->select();
+        $data = Db::table('user')->select();  //含前缀
         //echo json_encode($data);
         return json($data);
+    }
+
+    /**
+     * 查询数据 2
+     */
+    public function dbFind2()
+    {
+        $data = Db::name('user')->select();
+        echo json_encode($data);
+    }
+
+    /**
+     * 条件查询 3
+     */
+    public function dbFind3()
+    {
+        $User = new User(); // 实例化User对象
+//        $User->where('age=23 AND name="dhr"')->select();
+//        echo json_encode($User);
+        $User->setName("dddd");
+        echo json_encode($User);
+    }
+
+
+    /**
+     *添加一条数据 1
+     */
+    public function dbInsert1()
+    {
+        $data = [
+            'name' => '张三',
+            'age' => '20',
+        ];
+        $res = Db::name('user')->insert($data);
+        echo $res;
+    }
+
+    /**
+     *更新一条数据 1
+     */
+    public function dbUpdate1()
+    {
+        $res = Db::name('user')->where('id', 1)->update(['name' => '李d']);
+        echo $res;
+    }
+
+    /**
+     *更新一条数据 2
+     */
+    public function dbUpdate2()
+    {
+        $where[] = ['id', '>', 2];
+        $res = Db::name('user')->where($where)->update(['name' => '李四']);
+        echo $res;
+    }
+
+    /**
+     *更新一条数据 3
+     */
+    public function dbUpdate3()
+    {
+
     }
 }
--- application/admin/model/User.php
+++ application/admin/model/User.php
@@ -13,17 +13,77 @@ use think\Model;
 
 class User extends Model
 {
-    protected $pk = 'uid';
-
     protected $id;
 
     protected $name;
 
     protected $age;
 
+    /**
+     * User constructor.
+     * @param $id
+     * @param $name
+     * @param $age
+     */
+    public function __construct($id, $name, $age)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->age = $age;
+    }
+
     // 模型初始化
     protected static function init()
     {
         //TODO:初始化内容
     }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
 }
